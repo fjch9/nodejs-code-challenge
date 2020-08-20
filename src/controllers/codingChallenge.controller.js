772465e469f97a71b59526e3f9530cb98ea0d691
Frank Chaves <fjch.salazar@gmail.com>
2020-08-20 11:49:57 -0600

    testing csv upload

--- src/controllers/codingChallenge.controller.js
+++ src/controllers/codingChallenge.controller.js
@@ -1,44 +1,66 @@
+const csvtojson = require("csvtojson");
+
 const db = require("../models");
 const CsvUpload = db.csvUploads;
 
 // Uploads CSV content
 exports.upload = (req, res) => {
-  // Validate request
-  if (
-    !req.body.uuid ||
-    !req.body.vin ||
-    !req.body.make ||
-    !req.body.model ||
-    !req.body.mileage ||
-    !req.body.year ||
-    !req.body.price ||
-    !req.body.zip_code
-  ) {
-    res.status(400).send({ message: "Content can not be empty!" });
-    return;
+  try {
+    // Validate request
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded"
+      });
+    } else {
+      let csvFile = req.files.file;
+
+      //this will place the file into the upload directory
+      csvFile.mv("./uploads/" + csvFile.name);
+
+      res.send({
+        status: true,
+        message: "CSV file uploaded!",
+        data: {
+          name: csvFile.name,
+          mimetype: csvFile.mimetype,
+          size: csvFile.size
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
   }
 
-  const csvUpload = new CsvUpload({
-    uuid: req.body.uuid,
-    vin: req.body.vin,
-    make: req.body.make,
-    model: req.body.model,
-    mileage: req.body.mileage,
-    year: req.body.year,
-    price: req.body.price,
-    zip_code: req.body.zip_code
-  });
+  // const csvUpload = new CsvUpload({
+  //   uuid: req.body.uuid,
+  //   vin: req.body.vin,
+  //   make: req.body.make,
+  //   model: req.body.model,
+  //   mileage: req.body.mileage,
+  //   year: req.body.year,
+  //   price: req.body.price,
+  //   zip_code: req.body.zip_code
+  // });
 
   // Saves the data in the collection
-  csvUpload
-    .save(csvUpload)
-    .then(data => {
-      console.log(data);
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while uploading the data."
-      });
-    });
+  //   csvUpload
+  //     .save(csvUpload)
+  //     .then(data => {
+  //       console.log(data);
+  //       res.send(data);
+  //     })
+  //     .catch(err => {
+  //       res.status(500).send({
+  //         message: err.message || "Some error occurred while uploading the data."
+  //       });
+  //     });
+
+  //   csvtojson()
+  //     .fromFile(`./${req.files.file.name}`)
+  //     .then(csvData => {
+  //       console.log(csvData);
+  //     });
+
+  //   res.send(req.files);
 };
