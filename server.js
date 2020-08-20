1c4f44ce508c92a8712449a2e3f541f6b0048961
Frank Chaves <fjch.salazar@gmail.com>
2020-08-20 08:13:59 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json - application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// generic route
+app.get("/", (req, res) => {
+  res.json({ message: "Nodejs Code Challenge" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
